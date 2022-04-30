b0c1c19fbbe268291e17210784d6747968a41b23
shavaun1 <jshavaun@yahoo.com>
2022-04-29 23:38:53 -0400

    Commiting

--- Sorting.cpp
+++ Sorting.cpp
@@ -1,17 +1,26 @@
 #include <array>
 #include <iostream>
 
+
+
 using std::endl;
 using std::cout;
 using std::cin;
 using namespace std;
 
 void insert(int a[], int n) {
-	
+
+	srand((unsigned)time(NULL));
+	for (int i = 1; i <= n; i++) {
+		int ran = 1 + (rand() % 100);
+	 }
+
+
+	cout << "lowest to higest \n";
 	for (int i = 0; i < n; i++) {
 		int pop = i;
-		
-		
+
+
 		for (int j = i + 1; j < n; j++) {
 
 			//from lower to higher number " < ".
@@ -23,16 +32,64 @@ void insert(int a[], int n) {
 		a[i] = a[pop];
 		a[pop] = hold;
 
+		
 		cout << a[i] << " ";
+		
 	}
+	cout << " \n";
 }
 
 
+/// <summary>
+// Link list 
+/// </summary>
+
+// Make a node.
+struct Node {
+	int data;				//hold the integer value.
+	struct Node *next;		//	A node pointer
+};
+
+//display values in the node.
+void print_list(Node* n) {
+	while (n != NULL) {
+		cout << n->data << " ";
+		n = n->next;
+	}
+}
+
 int main() {
 
-	int c = 4;
-	int b[] = { 500,2000,11010,99 };
+	//creating the *link list*
+	Node* head = NULL;
+	Node* second = NULL;
+	Node* third = NULL;
 
+	head = new Node();
+	second = new Node();
+	third = new Node();
+
+	//putting data value and pointer in to each node.
+	head-> data = 1;
+	head-> next = second;
+
+	second->data = 2;
+	second-> next = third;
+
+	//the third node(last node-tail) should be pointed to NULL. 
+	third->data = 3;
+	third->next = NULL;
+
+	
+	print_list(head);
+
+	
+#if 0
+	//The insertion sort
+	int c = 4;
+	int b[] = {9,6,7,4};
+	
 	insert(b, c);
 
+#endif
 }
